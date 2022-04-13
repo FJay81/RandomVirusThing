30a6834f491be3aef0b4789f7adfb449855408fa
Faysal <82079632+FJay81@users.noreply.github.com>
2022-04-13 10:52:16 +0100

    Update VirusReWrite_txt.py

--- Virus/VirusReWrite_txt.py
+++ Virus/VirusReWrite_txt.py
@@ -1,6 +1,6 @@
-from VirusTool import Naming
+from VirusTool import VT
 import glob
 text = str(input("What would you like to write"))
 for files in glob.glob("*.txt"):
-    file = Naming(files)
+    file = VT(files)
     file.change(text)
